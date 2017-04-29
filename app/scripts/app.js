1f7bac200d82cdff8348ebc89a993e7c56d57ade
Robert Morawski <rjmorawski@yahoo.com>
2017-04-29 16:02:16 -0400

    Assignment 3 routing states with updated links

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,20 +4,24 @@
             .html5Mode({
                 enabled: true,
                 requireBase: false
-        });
+            });
         $stateProvider
             .state('landing', {
                 url: '/',
-            templateUrl: '/templates/landing.html'
-        })
-        .state('album', {
-            url: '/album',
-            templateUrl: '/templates/album.html'
-        });
+                templateUrl: '/templates/landing.html'
+            })
+            .state('album', {
+                url: '/album',
+                templateUrl: '/templates/album.html'
+            })
+            .state('collection', {
+                url: '/collection',
+                templateUrl: '/templates/collection.html'
+            });
         
     }
 
-angular
-    .module('blocJams', ['ui.router'])
-    .config(config);
+    angular
+        .module('blocJams', ['ui.router'])
+        .config(config);
 })();
