12858bc2d8494d1ebe06f8c28d4a5ac329fa71f7
Robert Morawski <rjmorawski@yahoo.com>
2017-05-04 17:36:47 -0400

    Assignment 7 angular services 2 complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -26,14 +26,18 @@
             currentSong = song;
         };
         
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+        
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
+                    playSong(song);
                 }
             }
         };
